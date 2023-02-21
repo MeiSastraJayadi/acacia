8f2967a4661833aba228bcf7eff2f6c4e7ee6c96
MeiSastraJayadi <kadekmeisastra13@gmail.com>
2023-02-21 09:23:27 +0800

    create router struct

--- multiplexer/router.go
+++ multiplexer/router.go
@@ -1,16 +1,35 @@
 package multiplexer
 
+import "net/http"
+
 type Router struct {
+  label string
+  prefix string
+  method []string
+  child []*Router
 }
 
 func NewRouter() *Router {
   return &Router{}
 }
 
-func (rt *Router) Methods(method ...string) *Router {
-  return rt
+func (rt *Router) Methods(methods ...string) *Router {
+  newRoute := NewRouter()
+  for _, m := range methods {
+    newRoute.method = append(newRoute.method, m)
+  }
+  rt.child = append(rt.child, newRoute)
+  return rt 
 }
 
 func (rt *Router) SubRouter() *Router {
-  return rt
+  return rt.child[len(rt.child)-1]
 }
+
+func (rt *Router) Handle(path string, handler http.Handler) {
+}
+
+func (rt *Router) HandleFunc(path string, handler http.HandlerFunc) {
+}
+
+
