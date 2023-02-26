8cabbdf8550a4079150a9b45a65996dd29508774
MeiSastraJayadi <kadekmeisastra13@gmail.com>
2023-02-26 13:42:40 +0800

    add some changes in servehttp

--- multiplexer/handler.go
+++ multiplexer/handler.go
@@ -35,7 +35,7 @@ func (rt *Router) HandleFunc(path string, handlerFunc http.HandlerFunc) {
 }
 
 func (rt *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) error {
-  url := r.URL.RawPath 
+  url := r.URL.Path
   method := r.Method
   handler, err := rt.tree.search(url,method)
   if err != nil {
