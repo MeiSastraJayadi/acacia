206639334f553d88c5206fe24d8128db9846577d
MeiSastraJayadi <kadekmeisastra13@gmail.com>
2023-02-27 10:35:50 +0800

    create some changes in middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -6,18 +6,38 @@ import (
 	"github.com/MeiSastraJayadi/acacia/multiplexer"
 )
 
+
+// UseMiddleware struct can create an object to apply UseMiddleware
+// in specific *multiplexer.Router object. This struct has two field. 
+// The first field is : ---> function, which is a Middleware Function
+// The second one is : --> router, which is a *multiplexer.Router where the middleware will be applied
 type UseMiddleware struct {
   function Middleware
   router *multiplexer.Router
 } 
 
+// Middleware is a function that need one parameter with type of http.Handler 
+// and will return a http.Handler
 type Middleware func(next http.Handler) http.Handler
 
-func NewUseMiddleware(router *multiplexer.Router, middleware Middleware) *UseMiddleware {
+// NewUseMiddleware will return UseMiddleware object
+func NewUseMiddleware() *UseMiddleware {
   return &UseMiddleware{
-    function: middleware,
-    router: router,
+    function: nil,
+    router: nil,
   }
+}
+
+// AddRouter function will add new *multiplexer.Router into the 
+// UseMiddleware object
+func (um *UseMiddleware) AddRouter(router *multiplexer.Router) {
+  um.router = router
+}
+
+// AddMiddleware function will add new Middleware function into the 
+// UseMiddleware object
+func (um *UseMiddleware) AddMiddleware(middleware Middleware) {
+  um.function = middleware 
 }
 
 func (mw *UseMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
